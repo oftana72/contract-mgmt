de79a5d1b83b0144192985b14ec2df4f71e95c8b
oftana72 <oftana1895@gmail.com>
2026-07-29 12:29:31 +0300

    Add PG Submit Days column to PO table

--- app.py
+++ app.py
@@ -149,6 +149,14 @@ class PurchaseOrder(db.Model):
         delta = (date.today() - lc.opened_date).days
         return delta
 
+    @property
+    def pg_submit_days(self):
+        pg = self.performance_guarantees.first()
+        if not pg or not pg.requested_date or not pg.received_date:
+            return None
+        delta = (pg.received_date - pg.requested_date).days
+        return delta
+
 class LineItem(db.Model):
     __tablename__ = 'line_items'
     id = db.Column(Integer, primary_key=True)
